c3604da40bef687bc19718937ddc3b76c3a239fc
wwf9w0 <aliemrekaradag1@gmail.com>
2022-03-13 13:30:08 +0300

    added email valid func

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/events"
 	"os"
+	"rest-api/handlers"
 )
 
 var (
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func GetUser() (*events.APIGatewayProxyResponse, error) {
+
+	return nil, nil
+}
+
+func CreateUser() (*events.APIGatewayProxyResponse, error) {
+	return nil, nil
+
+}
+
+func UpdateUser() (*events.APIGatewayProxyResponse, error) {
+	return nil, nil
+
+}
+
+func DeleteUser() (*events.APIGatewayProxyResponse, error) {
+	return nil, nil
+
+}
+
+func UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
+
+}
